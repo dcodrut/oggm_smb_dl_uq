17e0ab6534b392a26ec1ed622cdfd2d55d8108df
dcodrut <codrutandreidiaconu@gmail.com>
2023-07-25 16:38:07 +0200

    fix the meteorological data period; refactoring

--- smb_dl/main_generate_data.py
+++ smb_dl/main_generate_data.py
@@ -15,11 +15,6 @@ from pathlib import Path
 
 import config as local_cfg
 
-wd = local_cfg.OGGM_WD
-assert Path(wd).exists()
-
-cfg.PATHS['working_dir'] = wd
-
 
 def prepare_smb_df(fp):
     df = pd.read_csv(fp)
@@ -37,8 +32,8 @@ def prepare_smb_df(fp):
 
     # use sin(aspect) and cos(aspect) as features
     i_col = list(df_annual.columns).index('aspect')
-    df_annual.insert(loc=i_col+1, column='aspect_sin', value=np.sin(df_annual.aspect.values))
-    df_annual.insert(loc=i_col+2, column='aspect_cos', value=np.cos(df_annual.aspect.values))
+    df_annual.insert(loc=i_col + 1, column='aspect_sin', value=np.sin(df_annual.aspect.values))
+    df_annual.insert(loc=i_col + 2, column='aspect_cos', value=np.cos(df_annual.aspect.values))
     del df_annual['aspect']
 
     # mm to m
@@ -55,43 +50,41 @@ if __name__ == '__main__':
     gdf_all = gpd.read_file(fp)
     gdf_all = gdf_all.sort_values('Area', ascending=False)
 
+    Path(local_cfg.OGGM_WD).mkdir(exist_ok=True, parents=True)
+    cfg.PATHS['working_dir'] = local_cfg.OGGM_WD
     cfg.PARAMS['continue_on_error'] = True
     cfg.PARAMS['use_intersects'] = False
     cfg.PARAMS['border'] = 80
-
-    overwrite_gdirs = False
 
     num_glaciers = 500  # how many glaciers to simulate (the largest in the region will be used)
     crt_k = 1
     i = 0
     pbar = tqdm(total=num_glaciers)
+    init_from_prepro_level = True
     while crt_k <= num_glaciers:
         if i == len(gdf_all):
             break
-        gid = gdf_all.RGIId.iloc[i]
+        r = gdf_all.iloc[i]
+        gid = r.RGIId
         i += 1
         print(f'k = {crt_k}; gid = {gid}')
         sgdf = gdf_all[gdf_all.RGIId.isin([gid])]
         print(sgdf)
 
         # download level-3 data if needed
-        if overwrite_gdirs:
-            gdirs = workflow.init_glacier_directories(sgdf, from_prepro_level=3)
+        if init_from_prepro_level:
+            base_url = 'https://cluster.klima.uni-bremen.de/~oggm/gdirs/oggm_v1.6/L3-L5_files/2023.3/elev_bands/W5E5'
+            gdirs = workflow.init_glacier_directories(rgidf=[gid], from_prepro_level=3, prepro_base_url=base_url)
         else:
-            gdirs = workflow.init_glacier_directories(sgdf)
-        print(gdirs)
+            gdirs = workflow.init_glacier_directories(rgidf=[gid])
         gdir = gdirs[0]
 
-        # get the climate data
+        # read the climate data
         fp_clim = Path(gdir.get_filepath('climate_historical'))
         ds_ch = xr.open_dataset(fp_clim)
 
         # build the MB model (skipp the current glacier if it fails)
-        try:
-            mbmod = massbalance.MultipleFlowlineMassBalance(gdir)
-        except Exception as e:
-            print(e)
-            continue
+        mbmod = massbalance.MultipleFlowlineMassBalance(gdir)
 
         # compute the specific MB for both the original input data and the noisy version
         fls = gdir.read_pickle('model_flowlines')
@@ -99,7 +92,7 @@ if __name__ == '__main__':
         years = np.arange(y0, y1)
         mb_ts_orig = mbmod.get_specific_mb(fls=fls, year=years)
 
-        ds_ch_sel = ds_ch.where(ds_ch.time >= np.datetime64(f'{y0 - 1}-10-01'), drop=True)
+        ds_ch_sel = ds_ch.where(ds_ch.time >= np.datetime64(f'{y0}-01-01'), drop=True)
         dates = [pd.to_datetime(d, format='%Y-%m-d') for d in ds_ch_sel.time.values]
         years_fs = mbmod.flowline_mb_models[0].years
         df = pd.DataFrame({
@@ -112,14 +105,14 @@ if __name__ == '__main__':
         })
 
         # add some features from RGI
-        df['lon'] = sgdf.CenLon.iloc[0]
-        df['lat'] = sgdf.CenLat.iloc[0]
-        df['area'] = sgdf.Area.iloc[0]
-        df['z_min'] = sgdf.Zmin.iloc[0]
-        df['z_max'] = sgdf.Zmax.iloc[0]
-        df['z_med'] = sgdf.Zmed.iloc[0]
-        df['slope'] = sgdf.Slope.iloc[0]
-        df['aspect'] = sgdf.Aspect.iloc[0]
+        df['lon'] = r.CenLon
+        df['lat'] = r.CenLat
+        df['area'] = r.Area
+        df['z_min'] = r.Zmin
+        df['z_max'] = r.Zmax
+        df['z_med'] = r.Zmed
+        df['slope'] = r.Slope
+        df['aspect'] = r.Aspect
 
         fp = Path(f'{local_cfg.OGGM_OUT_DIR}/per_glacier/{gid}.csv')
         fp.parent.mkdir(parents=True, exist_ok=True)
